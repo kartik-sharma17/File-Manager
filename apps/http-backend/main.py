843eb0455e9c7ce9932225fb5e184d69dcda4851
Kartik Sharma <kartiksharma88103@gmail.com>
2026-08-28 00:33:53 +0530

    configure cors

--- apps/http-backend/main.py
+++ apps/http-backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000","https://file-manager-web-psi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
